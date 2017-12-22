66e2a94ed1d376f3ca56457843906e64a8a6079e
FreeBreadsticks <erika.hughes@dbspublishing.com>
2017-12-22 00:20:47 -0500

    Finished functionality for PokemonPage

--- pokedex-client/src/actions/actionTypes.js
+++ pokedex-client/src/actions/actionTypes.js
@@ -1,2 +1,4 @@
 export const LOAD_POKEMON_SUCCESS = 'LOAD_POKEMON_SUCCESS';
 export const SAVE_POKEMON_SUCCESS = 'SAVE_POKEMON_SUCCESS';
+
+  const myPoke = {description: "There is a large flower on Venusaur’s back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower’s aroma soothes the emotions of people.", id: 13, img_url: "https://img.pokemondb.net/artwork/venusaur.jpg", name: "Venasaur", poke_type: "Grass", pokedex_num: 3}
--- pokedex-client/src/components/PokemonPage.js
+++ pokedex-client/src/components/PokemonPage.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 const PokemonPage = ({pokemon}) => {
-  const myPoke = {description: "There is a large flower on Venusaur’s back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower’s aroma soothes the emotions of people.", id: 13, img_url: "https://img.pokemondb.net/artwork/venusaur.jpg", name: "Venasaur", poke_type: "Grass", pokedex_num: 3}
+
   return (
     <div className="container-fluid">
-      <h1 className="poke-title">{myPoke.name}</h1>
-      <h4 className="poke-title App">Pokedex No: {myPoke.pokedex_num}</h4>
+      <h1 className="poke-title">{pokemon.name}</h1>
+      <h4 className="poke-title App">Pokedex No: {pokemon.pokedex_num}</h4>
       <hr />
       <div className="poke-page-box">
-        <img src={myPoke.img_url} alt="" className="col-sm-4"/>
-        <p className="col-sm-6 poke-word">{myPoke.description}</p>
-        <p className="col-sm-2"><span className={'page-type poke-word ' + myPoke.poke_type}> {myPoke.poke_type}</span></p>
+        <img src={pokemon.img_url} alt="" className="col-sm-4"/>
+        <p className="col-sm-6 poke-word">{pokemon.description}</p>
+        <p className="col-sm-2"><span className={'page-type poke-word ' + pokemon.poke_type}> {pokemon.poke_type}</span></p>
       </div>
 
       <div className="row">
